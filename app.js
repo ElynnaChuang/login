66206e390ef3028d86cf505944bc07a269644634
yichennn <yichennn1010@gmail.com>
2022-11-25 18:15:30 +0800

    使用 express-session 製作「保存使用者登入狀態」

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
+const session = require('express-session')//儲存認證結果
 // require('./config/mongoose') //暫時不用連線
 const routes = require('./routes/routes')
 const app = express()
@@ -7,8 +8,15 @@ const port = 3000
 
 app.engine('hbs', exhbs({ extname: '.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
+app.set('trust proxy', 1)
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'secret',
+  name: 'user',//存放在cookie的key，預設是connect.sid
+  resave: true,
+  saveUninitialized: false,
+}))
 app.use(routes)
 
 app.listen(port, () => {
--- routes/modules/login.js
+++ routes/modules/login.js
@@ -2,27 +2,52 @@ const express = require('express')
 const router = express.Router()
 const users = require('../../users.json')
 
-router.get('/', (req, res) => {
+//middleware，讓每個進入的req先經過這個function
+function auth (req, res, next) {
+  if (req.session.user) {
+  console.log('authenticated')
+  next()
+  } else {
+  console.log('not authenticated')
+  return res.redirect('/login')
+  }
+}
+
+router.get('/', auth, (req, res) => {
+  const firstName = req.session.user
+  res.render('welcome', { firstName })
+})
+
+router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/', (req, res) => {
+router.post('/login', (req, res) => {
   const { email, password } = req.body
-  const registerInfo = users.find(user => user.email === email)
-  if (!registerInfo) {
-    return res.render('login', { email, registerInfo: !registerInfo })
-  } else {
-    const emailChecked = registerInfo.password === password
-    if (!emailChecked) {
-      return res.render('login', { email, emailChecked: !emailChecked })
-    }
-    res.redirect('/' + registerInfo.firstName)
+  const user = users.find(user => user.email === email) //找出對應的user
+  const passwordCheck = ( !user ) ? false : user.password === password
+  if (!user) {
+    //若找不到user(email)，表示無註冊過，返回 login 頁面並在輸入框加入提示「電子郵件尚未註冊」
+    return res.render('login', { user: !user })
+
+  } else if( !passwordCheck ) {
+    //有找到email，表示註冊過，但密碼不正確，返回 login 頁面並在輸入框加入提示「密碼錯誤」
+    return res.render('login', { email, passwordCheck: !passwordCheck })
+
+  }else {
+    //密碼正確 在伺服器端把使用者資訊存入 session store，並生成session ID -> 給使用者session ID存放在瀏覽器的cookie(下次拿來索引)
+    // req.session.xxx ，xxx = 在cookies中的key
+    req.session.user = user.firstName
+    res.redirect('/')
   }
 })
 
-router.get('/:firstName', (req, res) => {
-  const firstName = req.params.firstName
-  res.render('welcome', { firstName })
+router.get('/logout', auth, (req, res) => {
+  //session內建清除session store
+  req.session.destroy(() => {
+    console.log('session destroyed')
+  })
+  res.redirect('/login')
 })
 
 module.exports = router
